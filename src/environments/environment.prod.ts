55d51a14a77700082c84dff76529df9340696e2f
Andres Villegas <thisisvillegas@gmail.com>
2023-05-20 22:54:21 -0500

    attempt 3 github actions bug

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,22 +1,24 @@
 import config from '../../auth_config.json';
 
-const { domain, clientId, audience, apiUri, appUri, errorPath } = config as {
+const { domain, clientId, authorizationParams: { audience }, apiUri, errorPath } = config as {
   domain: string;
   clientId: string;
-  audience?: string;
+  authorizationParams: {
+    audience?: string;
+  },
   apiUri: string;
-  appUri: string;
   errorPath: string;
 };
 
 export const environment = {
-  production: true,
-  appUri: appUri,
+  production: false,
   auth: {
     domain,
     clientId,
-    ...(audience && audience !== "YOUR_API_IDENTIFIER" ? { audience } : null),
-    redirectUri: window.location.origin,
+    authorizationParams: {
+      ...(audience && audience !== 'YOUR_API_IDENTIFIER' ? { audience } : null),
+      redirect_uri: window.location.origin,
+    },
     errorPath,
   },
   httpInterceptor: {
